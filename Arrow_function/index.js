3481e9ba9c9e9d11a62d62aa6ebf3027ca465bc8
kalandhar <kalandars2004@gmail.com>
2024-09-13 19:26:51 +0530

    js

--- Arrow_function/index.js
+++ Arrow_function/index.js
@@ -1,33 +1,43 @@
-function madhan() {
-    console.log("Madhan Maadu");
+// function madhan() {
+//     console.log("Madhan Maadu");
+// }
+// madhan();
+
+// const madhan1 = () => {
+//     console.log("Madhan Maadu i am from arrow function");
+// }
+
+// madhan1();
+
+
+// const add = (a,b) =>{
+//     let check = a + b;
+//     return check;
+// }
+// let res = add(10,15);
+// console.log(res);
+
+
+// const sub = (a,b) => {return a-b} ;
+
+// // console.log(sub(10,15));
+// let ans  = sub(10,15);
+
+
+// //////no parameter
+
+// const nopar = (a,b,c) => {
+//     console.log(a,b,c);
+// }
+
+// nopar(10);
+
+
+let loop = (n) => {
+    for(let i = 1; i <=n; i++)
+    {
+        console.log(i);
+    }
 }
-madhan();
 
-const madhan1 = () => {
-    console.log("Madhan Maadu i am from arrow function");
-}
-
-madhan1();
-
-
-const add = (a,b) =>{
-    let check = a + b;
-    return check;
-}
-let res = add(10,15);
-console.log(res);
-
-
-const sub = (a,b) => {return a-b} ;
-
-// console.log(sub(10,15));
-let ans  = sub(10,15);
-
-
-//////no parameter
-
-const nopar = (a,b,c) => {
-    console.log(a,b,c);
-}
-
-nopar(10);
+loop(10);
