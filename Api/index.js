1c80b0a740d9aa4a8cd8616182fefcd0584ec8bd
kalandhar <kalandars2004@gmail.com>
2024-09-16 10:06:17 +0530

    Tailwind setup

--- Api/index.js
+++ Api/index.js
@@ -4,20 +4,20 @@ async function Apicall(){
     try{
     const res = await fetch('https://66e526c95cc7f9b6273c697f.mockapi.io/test/vs/user')
     const data = await res.json();
-    console.table(data);
+    // console.table(data);
     
 
-    const userhtml = data.map( user => {
-        return `
-        <p> Name : <strong>${user.name}</strong> </p>
-        <p> Index : ${user.id} </p>
+    // const userhtml = data.map( user => {
+    //     return `
+    //     <p> Name : <strong>${user.name}</strong> </p>
+    //     <p> Index : ${user.id} </p>
     
-        <p> Active : ${user.isActive} </p>
-        <br>`
-    });
+    //     <p> Active : ${user.isActive} </p>
+    //     <br>`
+    // });
 
-    document.getElementById('print').innerHTML = userhtml.join('');
-   
+    // document.getElementById('print').innerHTML = userhtml.join('');
+    return  data;
 
 
         }
@@ -27,11 +27,14 @@ async function Apicall(){
     
 }
 
-// async function API(){
-//     const apidata = await Apicall();
-//     console.table(apidata);
-// }
+async function API(){
+    const apidata = await Apicall();
+    const fil = apidata.filter((user) => user.id);
+    console.log(fil);
+
+    // console.table(apidata);
+}
 
 
 
-Apicall();
+API();
---
+++ Tailwind/test-1/src/component/Input.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Input = () => {
+  return (
+    <>
+    <div className='h-screen w-screen bg-slate-400 flex justify-center items-center'>
+      <div className='w-[30rem] h-[30rem] bg-white hover:shadow-inherit'>
+        <h1 className='text-center text-black font-extrabold text-3xl'>Form</h1></div>
+   
+    </div>
+      
+    </>
+  )
+}
+
+export default Input
